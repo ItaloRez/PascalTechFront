f22d4ca3eef89155e389ce48d60f90b1ce2edb69
Ítalo de Rezende <italoeji@hotmail.com>
2022-10-28 18:20:11 -0300

    adicionando scripts para google analitycs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
 import { CarrinhoProvider } from "../contexts/carrinho";
+import Script from "next/script";
 
 import "../styles/global.scss";
 import "react-toastify/dist/ReactToastify.css";
@@ -18,6 +19,22 @@ function MyApp({ Component, pageProps }: AppProps) {
       </CarrinhoProvider>
 
       <ToastContainer />
+
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
     </>
   );
 }
