e3547a509f321657bc23b9d94f60cbfa4940d079
Ítalo de Rezende <italoeji@hotmail.com>
2022-10-28 18:22:54 -0300

    corrigindo scripts do google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,11 +21,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ToastContainer />
 
       <Script
+        id="analyticsGoogle"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="googleAnalytics">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
